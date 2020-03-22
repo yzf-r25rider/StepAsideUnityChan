5c50c5aa7e2966b3fd2fe01bcebff7b268158bb7
エクバ2勝てないマン <trumanzz7575@gmail.com>
2020-03-22 17:07:43 +0900

    Lesson6の課題

---
+++ Assets/ObjectD.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectD : MonoBehaviour {
+
+   
+    GameObject unitychan;
+
+	// Use this for initialization
+	void Start () {
+        //オブジェクトの位置を取得する。
+
+    
+       
+        unitychan = GameObject.Find("unitychan");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.gameObject.transform.position.z < unitychan.transform.position.z)
+        {
+         
+
+            Destroy(this.gameObject);
+
+        }
+
+
+
+	}
+}
